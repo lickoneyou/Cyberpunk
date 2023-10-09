04baac335014b16f42c6f2ac3805511a62bdab17
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2023-10-09 16:49:53 +0300

    completed header

--- cyberpunk/src/App.js
+++ cyberpunk/src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import "./App.css";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
