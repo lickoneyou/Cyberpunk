8e7671f6da51f80f3a2c4482e5e31126cc7403aa
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2023-10-10 11:20:46 +0300

    completed Advertising component

--- cyberpunk/src/Components/Main/Main.jsx
+++ cyberpunk/src/Components/Main/Main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import styles from "./Main.module.css";
 import NightCity from "./NigthCity/NightCity";
 import Promotion from "./Promotion/Promotion";
+import Advertising from "./Advertising/Advertising";
 
 const Main = () => {
   return (
     <main>
       <NightCity />
       <Promotion />
+      <Advertising />
     </main>
   );
 };
